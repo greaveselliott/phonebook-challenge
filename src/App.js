7dc2609cd34a361aa9c593e79206e05ed9cacf20
Elliott Greaves <greaveselliott@me.com>
2018-02-26 22:39:21 +0000

    wired up state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { filter } from 'benchmark';
 class App extends Component {
 
+  sortBy = ["Name", "Phone number", "Address"];
+
   state = {
+    original: [],
     contacts: [],
-    selectedContact: null
+    selectedContact: null,
+    typeValue: this.sortBy[0]
+  }
+
+  typeHandler (event) {
+    this.setState({typeValue: event.target.value});
+  }
+
+  searchHandler (event) {
+      const type = this.state.typeValue.toLowerCase();
+
+      let filtered = this.state.original.filter(contact => {
+          const value = contact[type].toLowerCase();
+          const term = event.target.value.toLowerCase();
+
+          return value.indexOf(`${term}`) > -1 || term.length <= 0; 
+      });
+      this.setState({contacts: filtered});
   }
 
   componentWillMount() {
      fetch("http://www.mocky.io/v2/581335f71000004204abaf83")
      .then((response) => { return response.json(); })
      .then((response) => {
-         this.setState({contacts: response.contacts})
+         this.setState({
+            original: response.contacts,
+            contacts: response.contacts,
+          })
      });
   }
 
@@ -19,8 +43,8 @@ class App extends Component {
     return (
       <main className="app">
         <Header>
-          <Sort/>
-          <Search/>
+          <Sort typeHandler={this.typeHandler.bind(this)} options={this.sortBy}/>
+          <Search searchHandler={this.searchHandler.bind(this)}/>
         </Header>
         <AddressBook contacts={this.state.contacts}/>
       </main>
@@ -28,26 +52,24 @@ class App extends Component {
   }
 }
 
-
-
 const AddressBook = ({contacts}) => {
-  return <ul className="address-book">{contacts.map(contact => <AddressContact contact={contact}/>)}</ul>
+  return <ul className="address-book">{contacts.map(contact => <AddressContact key={contact.name} contact={contact}/>)}</ul>
 };
 
-const AddressContact = ({contact}) => {
-  return  <li className="address-book__contact">
-              <figure className="address-book__avatar">
-                <img className="address-book__image" src="https://loremflickr.com/100/100" alt={`${contact.name}`} itemprop="image"/>
-              </figure>
-              <figcaption className="address-book__details">
-              <h3 className="address-book__name" itemprop="name">{contact.name}</h3>
-              <small className="address-book__phone" itemprop="telephone">{contact.phone_number}</small>
-              <small className="address-book__address" itemprop="address">
-                {contact.address}
-              </small>
-              </figcaption>
-          </li>
-};
+const AddressContact = ({contact}) => (
+  <li className="address-book__contact">
+    <figure className="address-book__avatar">
+      <img className="address-book__image" src="https://loremflickr.com/100/100" alt={`${contact.name}`} itemProp="image"/>
+    </figure>
+    <figcaption className="address-book__details">
+      <h3 className="address-book__name" itemProp="name">{contact.name}</h3>
+      <small className="address-book__phone" itemProp="telephone">{contact.phone_number}</small>
+      <small className="address-book__address" itemProp="address">
+        {contact.address}
+      </small>
+    </figcaption>
+</li>
+);
 
 const Header = ({children}) => (
   <div className="header">
@@ -55,24 +77,24 @@ const Header = ({children}) => (
   </div>
 );
 
-const Sort = () => (
-  <div className="header__sort">
-    <label className="header__label">Sort</label>
-    <select className="select-field">
-      <option value="name">Name</option>
-      <option value="phone">Phone</option>
-      <option value="address">Address</option>
+const Sort = ({typeHandler, options}) => {
+  return (
+    <div className="header__sort">
+      <label className="header__label">Sort</label>
+      <select onChange={typeHandler} className="select-field">
+        {
+          options.map(option => (
+            <option key={option} value={option.replace(/ /g,"_")}>{option}</option>
+          ))
+        }
     </select>
-    <select className="select-field">
-      <option value="Asc">A-Z</option>
-      <option value="Dsc">Z-A</option>
-    </select>
-  </div>
-);
+    </div>
+  );
+}
 
-const Search = () => (
+const Search = ({searchHandler}) => (
   <div className="header__search">
-    <input type="text" className="input-field"/>
+    <input type="text" className="input-field" onChange={searchHandler}/>
     <input type="submit" value="Search" className="submit-button"/>
   </div>
 );
